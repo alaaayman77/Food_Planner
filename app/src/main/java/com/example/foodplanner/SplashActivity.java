c36c79aa6c18ed7f60dbee6b90c81f81e9e2c240
alaaayman77 <alaaaymanfouad@gmail.com>
2026-01-30 19:16:46 +0200

    splash screen ui

---
+++ app/src/main/java/com/example/foodplanner/SplashActivity.java
@@ -0,0 +1,71 @@
+package com.example.foodplanner;
+import android.animation.ObjectAnimator;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.OvershootInterpolator;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+public class SplashActivity extends AppCompatActivity {
+Typeface tf;
+TextView slogan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+         tf = ResourcesCompat.getFont(this, R.font.fredoka_bold);
+         slogan = findViewById(R.id.tv_slogan);
+        if (tf != null) slogan.setTypeface(tf);
+        View logo = findViewById(R.id.logo);
+
+        logo.setScaleX(0.3f);
+        logo.setScaleY(0.3f);
+        logo.setAlpha(0f);
+
+        startLogoAnimation(logo);
+    }
+
+    private void startLogoAnimation(View logo) {
+        ObjectAnimator scaleX = ObjectAnimator.ofFloat(logo, "scaleX", 0.3f, 1.25f, 0.95f, 1f);
+        ObjectAnimator scaleY = ObjectAnimator.ofFloat(logo, "scaleY", 0.3f, 1.25f, 0.95f, 1f);
+        ObjectAnimator alpha = ObjectAnimator.ofFloat(logo, "alpha", 0f, 1f);
+        ObjectAnimator drop = ObjectAnimator.ofFloat(logo, "translationY", -50f, 20f, 0f);
+
+        scaleX.setDuration(900);
+        scaleY.setDuration(900);
+        alpha.setDuration(900);
+        drop.setDuration(900);
+
+        scaleX.setInterpolator(new OvershootInterpolator(3f));
+        scaleY.setInterpolator(new OvershootInterpolator(3f));
+        alpha.setInterpolator(new AccelerateDecelerateInterpolator());
+        drop.setInterpolator(new AccelerateDecelerateInterpolator());
+
+        scaleX.start();
+        scaleY.start();
+        alpha.start();
+        drop.start();
+
+        drop.addListener(new android.animation.AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(android.animation.Animator animation) {
+                startFloatingAnimation(logo);
+            }
+        });
+    }
+
+    private void startFloatingAnimation(View view) {
+        ObjectAnimator animator =
+                ObjectAnimator.ofFloat(view, "translationY", 0f, -15f, 0f);
+        animator.setDuration(2000);
+        animator.setRepeatCount(ObjectAnimator.INFINITE);
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.start();
+    }
+}
